d2c6bf01ea37bcf166d7b4cd39c6b5819f8917de
Rob <rob@robs-imac.home>
2020-11-07 11:31:08 +0000

    Added SQL, DB and tables

---
+++ db/project_manager.sql
@@ -0,0 +1,42 @@
+DROP TABLE IF EXISTS employee_tasks;
+DROP TABLE IF EXISTS employee_projects;
+DROP TABLE IF EXISTS tasks;
+DROP TABLE IF EXISTS projects;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS companies;
+
+CREATE TABLE companies (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255)
+);
+
+CREATE TABLE employees (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    company INT REFERENCES companies(id)
+);
+
+CREATE TABLE projects (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    company INT REFERENCES companies(id)
+);
+
+CREATE TABLE tasks (
+    id SERIAL PRIMARY KEY,
+    name  VARCHAR(255),
+    details TEXT,
+    project_id INT REFERENCES projects(id)
+);
+
+CREATE TABLE employee_projects (
+    id SERIAL PRIMARY KEY,
+    employee_id INT REFERENCES employees(id),
+    project_id INT REFERENCES projects(id)
+);
+
+CREATE TABLE employee_tasks (
+    id SERIAL PRIMARY KEY,
+    employee_id INT REFERENCES employees(id),
+    task_id INT REFERENCES tasks(id)
+);
