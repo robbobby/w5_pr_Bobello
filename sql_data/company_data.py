21cc029a756dc0452c9ecd9c00aeb6650986351b
Rob <rob@robs-imac.home>
2020-11-07 15:51:12 +0000

    SQL queries done for companies table

---
+++ sql_data/company_data.py
@@ -0,0 +1,38 @@
+from db.run_sql import run_sql
+from models.company import Company
+
+def add(company):
+    query = 'INSERT INTO companies (name) VALUES (%s) RETURNING *;'
+    values = company.id
+    results = run_sql(query, values)
+    company.id = results[0]['id']
+
+def select_all():
+    companies = []
+    query = 'SELECT * FROM companies'
+    results = run_sql(query)
+    for company in results:
+        company_to_add = Company(company['name'], company['id'])
+        companies.append(company_to_add)
+
+    return companies
+
+def select(company_id):
+    query = "Select * FROM companies WHERE id = %s"
+    results = run_sql(query, [company_id])
+    company = Company(results['name'], results['id'])
+    return company
+
+
+def delete(company):
+    query = 'DELETE FROM companies WHERE id = %s'
+    values = [company.id]
+    run_sql(query, values)
+
+def update(company):
+    query = 'UPDATE companies SET name = %s WHERE id = %s'
+    values = [company.name, company.id]
+    run_sql(query, values)
+
+def delete_all():
+    run_sql('DELETE FROM companies')
